3147552c4164fce7e7affb91c6d5ebcf7ce4c781
JefvdA <jef.v.d.a@live.be>
2023-03-12 18:39:19 +0100

    feat: added get all characters query

--- src/characters.resolver.ts
+++ src/characters.resolver.ts
@@ -3,12 +3,28 @@ import {Character} from "./character";
 
 @Resolver(Character)
 export class CharactersResolver {
-    @Query(returns => Character, { name: 'character'})
+    @Query(returns => Character, { name: 'character' })
     getCharacter(@Args('id', { type: () => Int }) id: number) {
         return new Character(
             id,
             'Joel',
             'Miller'
         );
+    }
+
+    @Query(returns => [Character], { name: 'character' })
+    getCharacters() {
+        return [
+            new Character(
+                1,
+                'Joel',
+                'Miller',
+            ),
+            new Character(
+                2,
+                'Ellie',
+                'Williams',
+            ),
+        ];
     }
 }
