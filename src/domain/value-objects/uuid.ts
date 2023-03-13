d8aae3e227fea8c6b80ea4070c6dacc8abba34e7
JefvdA <jef.v.d.a@live.be>
2023-03-13 23:16:20 +0100

    feat: added domain layer
    Refs: #15

---
+++ src/domain/value-objects/uuid.ts
@@ -0,0 +1,15 @@
+import ValueObjectValidationError from '../errors/value-object-validation-error';
+
+export default class Uuid {
+  readonly uuidRegex: RegExp =
+    /^[a-f\d]{8}-[a-f\d]{4}-[a-f\d]{4}-[a-f\d]{4}-[a-f\d]{12}$/i;
+
+  constructor(readonly value: string) {
+    if (!this.isValidUuid(value))
+      throw new ValueObjectValidationError('Uuid', 'Invalid uuid');
+  }
+
+  private isValidUuid(uuid: string): boolean {
+    return this.uuidRegex.test(uuid);
+  }
+}
