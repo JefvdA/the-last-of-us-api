f2ab37074b3c508d47819a383d32a86719a25ba4
JefvdA <jef.v.d.a@live.be>
2023-03-14 23:12:14 +0100

    feat/test: added NotFoundError + tests
    Refs: #19

---
+++ src/domain/errors/not-found-error.ts
@@ -0,0 +1,8 @@
+export default class NotFoundError extends Error {
+    constructor(
+        public readonly objectName: string
+    ) {
+        super(`${objectName} was not found`);
+        this.name = this.constructor.name;
+    }
+}
