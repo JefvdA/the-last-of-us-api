33f7ee6425ab2d4944dcc6c3dc8f16df14e7d7c0
JefvdA <jef.v.d.a@live.be>
2023-03-14 19:58:44 +0100

    feat: created infrastructure layer module
    Refs: #15

---
+++ src/infrastructure/infrastructure.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import {GraphQLModule} from "@nestjs/graphql";
+import {ApolloDriver, ApolloDriverConfig} from "@nestjs/apollo";
+import {ApolloServerPluginLandingPageLocalDefault} from "@apollo/server/plugin/landingPage/default";
+import {CharactersResolver} from "../characters.resolver";
+import {CharactersService} from "../characters.service";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {CharacterEntity} from "../character.entity";
+
+@Module({
+    imports: [
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            autoSchemaFile: 'src/schema.gql',
+            sortSchema: true,
+            playground: false,
+            plugins: [ApolloServerPluginLandingPageLocalDefault()],
+        }),
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'db',
+            port: 3306,
+            username: 'root',
+            password: 'root',
+            database: 'the-last-of-us-db',
+            autoLoadEntities: true,
+        }),
+        TypeOrmModule.forFeature([CharacterEntity]),
+    ],
+    providers: [CharactersResolver, CharactersService]
+})
+export class InfrastructureModule {}
