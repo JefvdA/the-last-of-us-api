89c4d406fccc4fdce2e679a0eb7120c53c24d7d6
JefvdA <jef.v.d.a@live.be>
2023-03-19 22:40:27 +0100

    tests: added tests for GraphqlExceptionFilterMiddleware

--- src/presentation/middleware/graphql-exception-filter.middleware.ts
+++ src/presentation/middleware/graphql-exception-filter.middleware.ts
@@ -2,6 +2,8 @@ import {ArgumentsHost, Catch, HttpStatus} from "@nestjs/common";
 import { GqlExceptionFilter } from "@nestjs/graphql";
 import { ApolloError } from "apollo-server-errors";
 import NotFoundError from "../../domain/errors/not-found-error";
+import ValueObjectValidationError from "../../domain/errors/value-object-validation-error";
+import {QueryFailedError} from "typeorm";
 
 @Catch()
 export default class GraphqlExceptionFilterMiddleware implements GqlExceptionFilter {
@@ -10,7 +12,14 @@ export default class GraphqlExceptionFilterMiddleware implements GqlExceptionFil
 
         switch (exception.constructor) {
             case NotFoundError:
-                statusCode = HttpStatus.NOT_FOUND
+                statusCode = HttpStatus.NOT_FOUND;
+                break;
+            case ValueObjectValidationError:
+                statusCode = HttpStatus.BAD_REQUEST;
+                break;
+            case QueryFailedError:
+                statusCode = HttpStatus.CONFLICT;
+                break;
         }
 
         return new ApolloError(exception.message, HttpStatus[statusCode], { statusCode: statusCode });
