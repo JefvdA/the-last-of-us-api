1032ec54e28c01a7a7b8c35a3fcd9801dcbf90d5
JefvdA <jef.v.d.a@live.be>
2023-03-19 18:09:31 +0100

    feat: added middleware module

--- src/presentation/presentation.module.ts
+++ src/presentation/presentation.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ResolversModule } from './resolvers/resolvers.module';
 import { SchemasModule } from './schemas/schemas.module';
 import { MappersModule } from './mappers/mappers.module';
+import { MiddlewareModule } from './middleware/middleware.module';
 
 @Module({
-  imports: [ResolversModule, SchemasModule, MappersModule],
+  imports: [ResolversModule, SchemasModule, MappersModule, MiddlewareModule],
 })
 export class PresentationModule {}
