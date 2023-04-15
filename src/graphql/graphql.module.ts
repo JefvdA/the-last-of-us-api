932b0ebc2a9a6378ed087cb96f9ae2411a3980bb
JefvdA <jef.v.d.a@live.be>
2023-04-15 19:03:55 +0200

    fix: fixed graphql exception filter

--- src/graphql/graphql.module.ts
+++ src/graphql/graphql.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { APP_FILTER } from '@nestjs/core';
+import GraphqlExceptionFilterMiddleware from './middleware/graphql-exception-filter.middleware';
 
 @Module({
   imports: [
@@ -12,6 +14,12 @@ import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin
       playground: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
+  ],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: GraphqlExceptionFilterMiddleware,
+    },
   ],
 })
 export default class GraphqlModule {}
